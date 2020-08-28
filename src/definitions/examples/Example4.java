4851de5b55047b367cfc5874d3373993ce3c562a
dbc2201 <divyanshb22@gmail.com>
2020-08-28 16:33:14 +0530

    add example for final class;eod

--- src/definitions/examples/Example4.java
+++ src/definitions/examples/Example4.java
@@ -8,12 +8,12 @@ package definitions.examples;
 
 public class Example4 {
     public static void main(String[] args) {
-        Bus miniBus = new Bus();
-        miniBus.methodA();
+//        Bus miniBus = new Bus();
+//        miniBus.methodA();
     }
 }
 
-class Vehicle {
+final class Vehicle {
     float speed;
     float mileage;
 
@@ -22,9 +22,9 @@ class Vehicle {
     }
 }
 
-class Bus extends Vehicle {
-    @Override
-    void methodA() {
-        System.out.println("Bus class method.");
-    }
-}
+//class Bus extends Vehicle {
+//    @Override
+//    void methodA() {
+//        System.out.println("Bus class method.");
+//    }
+//}
