6215ce18e87a37aec4d24c1c3f1cd683c56a9a9a
dbc2201 <divyanshb22@gmail.com>
2020-08-31 10:32:03 +0530

    add example for dynamic binding

--- src/definitions/bindings/DynamicBinding.java
+++ src/definitions/bindings/DynamicBinding.java
@@ -8,13 +8,20 @@ package definitions.bindings;
 
 public class DynamicBinding {
     public static void main(String[] args) {
-
+        Airplane myAirplane = new Jet();
+        // We can store the reference of a child class object, inside
+        // the reference variable of the parent class.
+        myAirplane.display();
+        myAirplane.showRemainingFuel();
     }
 }
 
 class Airplane {
     public void display() {
         System.out.println("Airplane Class.");
+    }
+    public void showRemainingFuel() {
+        System.out.println("Sufficient Fuel Remaining.");
     }
 }
 
