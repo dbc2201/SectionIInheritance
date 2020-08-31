b55b29325cf1b5880e5c0c63e5ff52b257645fc7
dbc2201 <divyanshb22@gmail.com>
2020-08-31 10:09:51 +0530

    create StaticBinding.java

---
+++ src/definitions/bindings/StaticBinding.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 31/08/20
+ *  Time: 10:09 AM
+ *  File Name : StaticBinding.java
+ * */
+package definitions.bindings;
+
+public class StaticBinding {
+}
