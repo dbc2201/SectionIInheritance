7d0b83a20522e5b7f02aad0a3c0c520e921f9830
dbc2201 <divyanshb22@gmail.com>
2020-09-03 15:06:52 +0530

    create Example.java

---
+++ src/definitions/abstractClass/Example.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 03/09/20
+ *  Time: 3:06 PM
+ *  File Name : Example.java
+ * */
+package definitions.abstractClass;
+
+public class Example {
+}
