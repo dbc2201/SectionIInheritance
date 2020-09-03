61634340cec9636bb2423acacdbafde5cbfdba27
dbc2201 <divyanshb22@gmail.com>
2020-09-03 15:15:40 +0530

    create Airplane.java

---
+++ src/definitions/abstractClass/Airplane.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 03/09/20
+ *  Time: 3:15 PM
+ *  File Name : Airplane.java
+ * */
+package definitions.abstractClass;
+
+public class Airplane {
+}
