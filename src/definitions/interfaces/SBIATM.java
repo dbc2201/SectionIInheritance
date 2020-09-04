dae610dfd76b41ce65ec854354e863ded5f8e900
dbc2201 <divyanshb22@gmail.com>
2020-09-04 16:17:55 +0530

    create SBIATM.java

---
+++ src/definitions/interfaces/SBIATM.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 04/09/20
+ *  Time: 4:17 PM
+ *  File Name : SBIATM.java
+ * */
+package definitions.interfaces;
+
+public class SBIATM {
+}
