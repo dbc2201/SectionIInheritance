3188f6cc85c65ea87e5d6bfb5d8f03d533cb51d9
dbc2201 <divyanshb22@gmail.com>
2020-09-04 16:08:17 +0530

    create ATM interface in ATM.java

---
+++ src/definitions/interfaces/ATM.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 04/09/20
+ *  Time: 4:08 PM
+ *  File Name : ATM.java
+ * */
+package definitions.interfaces;
+
+public interface ATM {
+}
