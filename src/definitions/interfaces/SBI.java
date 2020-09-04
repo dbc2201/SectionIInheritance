085425922759243b4ee2dc95f7c5031dd9f7bc24
dbc2201 <divyanshb22@gmail.com>
2020-09-04 16:09:41 +0530

    create class SBI

---
+++ src/definitions/interfaces/SBI.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 04/09/20
+ *  Time: 4:09 PM
+ *  File Name : SBI.java
+ * */
+package definitions.interfaces;
+
+public class SBI {
+}
